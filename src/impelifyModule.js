6947f94e6cf5177af1cb1bb87b9d61b3471b1ff1
Neil Breeden III <neil@htmlbuffet.com>
2015-04-14 14:03:02 -0700

    Removed requirejs

--- src/impelifyModule.js
+++ src/impelifyModule.js
@@ -1,16 +1,73 @@
-/*global document */
+/*global angular */
 
-define(['angular', 'src/impelifyDirective'], function(angular, impelifyDirective) {
-    var moduleName = 'impelifyModule',
-        module = angular.module(moduleName, []);
+//define(['angular', 'src/impelifyDirective'], function(angular, impelifyDirective) {
+//    var moduleName = 'impelifyModule',
+//        module = angular.module(moduleName, []);
+//
+//    module.directive(impelifyDirective.$name, impelifyDirective);
+//
+//    module.bootstrap = function () {
+//        angular.element(document).ready(function () {
+//            angular.bootstrap(document, [moduleName]);
+//        });
+//    };
+//
+//    return module;
+//});
 
-    module.directive(impelifyDirective.$name, impelifyDirective);
+var moduleName = 'impelifyModule';
 
-    module.bootstrap = function () {
-        angular.element(document).ready(function () {
-            angular.bootstrap(document, [moduleName]);
-        });
+var impelifyModule = angular.module(moduleName, []);
+
+impelifyModule.directive('impelify',function() {
+    function attachEvents(element, type, callback) {
+        var pfx = ["webkit", "moz", "MS", "o", ""];
+
+        for (var p = 0; p < pfx.length; p++) {
+            if (!pfx[p]) {
+                type = type.toLowerCase();
+            }
+
+            element.on(pfx[p] + type, callback);
+        }
+    }
+
+    return {
+        restrict: 'A',
+        scope: {
+            start: '&onStart',
+            end: '&onEnd',
+            transition: '&onTransition'
+        },
+        controller: ['$scope', function(scope) {
+            scope.startFunc = scope.start();
+            scope.endFunc = scope.end();
+            scope.transFunc = scope.transition();
+        }],
+        link: function(scope, element) {
+            function invokeFunc(func, event) {
+                if (func) {
+                    scope.$apply(function () {
+                        func(event);
+                    });
+                }
+            }
+
+            function animationStart(event) {
+                invokeFunc(scope.startFunc, event);
+            }
+
+            function animationEnd(event) {
+                invokeFunc(scope.endFunc, event);
+            }
+
+            function animationTransition(event) {
+                invokeFunc(scope.transFunc, event);
+            }
+
+            attachEvents(element, 'AnimationStart', animationStart);
+            attachEvents(element, 'AnimationEnd', animationEnd);
+            attachEvents(element, 'AnimationTransition', animationTransition);
+        }
     };
-
-    return module;
 });
